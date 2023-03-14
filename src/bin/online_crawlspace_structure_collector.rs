6ac24f4e1b26b1348ac394e114466b20bb32efe0
David <daniel.david.surla@gmail.com>
2023-03-14 23:14:18 +0000

    updated src\bin\online_crawlspace_structure_collector.rs

--- src/bin/online_crawlspace_structure_collector.rs
+++ src/bin/online_crawlspace_structure_collector.rs
@@ -31,13 +31,22 @@ pub struct DocumentStructure<T> {
     forms: Forms,
 }
 
+pub struct DocumentCrawler<T> {
+    t: T,
+    crawling_concurrency: usize,
+    processing_concurrency: usize,
+}
+
 #[rocket::async_trait]
 pub trait CrawlerHandler<Item> {
     fn name(&self) -> String;
     fn url(&self) -> Vec<String>;
     fn scrape(
         &self, url: &str,
-    ) -> Result<(&Item, std::string::String), Box<(dyn std::error::Error + 'static)>>; // (Vec<Item>, Vec<String>)
+    ) -> Result<
+        (&Item, std::string::String),
+        Box<(dyn std::error::Error + 'static)>,
+    >; // (Vec<Item>, Vec<String>)
     fn process(
         &self, item: Item,
     ) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>>;
@@ -50,10 +59,12 @@ impl<Item> CrawlerHandler<Item> for DocumentStructure<Item> {
 
     fn scrape(
         &self, url: &str,
-    ) -> Result<(&Item, std::string::String), Box<(dyn std::error::Error + 'static)>> {
+    ) -> Result<
+        (&Item, std::string::String),
+        Box<(dyn std::error::Error + 'static)>,
+    > {
         let t = &self.element;
         let s = self.url().into_iter().collect();
-
         Ok((t, s))
     }
 
@@ -61,6 +72,54 @@ impl<Item> CrawlerHandler<Item> for DocumentStructure<Item> {
         &self, item: Item,
     ) -> std::result::Result<(), std::boxed::Box<dyn std::error::Error>> {
         Ok(())
+    }
+}
+
+impl<X> DocumentCrawler<X> {
+    pub /* async */ fn run<T: Send + 'static>(
+        self: &Self, handler: Arc<dyn CrawlerHandler<T>>,
+    ) {
+        let mut visited_urls = std::collections::HashSet::<String>::new();
+        let crawling_concurrency = self.crawling_concurrency;
+        let crawling_queue_capacity = crawling_concurrency * 512;
+        let processing_concurrency = self.processing_concurrency;
+        let processing_queue_capacity = processing_concurrency * 8;
+        let active_crawlers = Arc::new(AtomicUsize::new(0));
+
+        let (urls_to_visit_x1, urls_to_visit_y1) =
+            tokio::sync::mpsc::channel::<T>(crawling_queue_capacity);
+        let (items_x1, items_y1) =
+            tokio::sync::mpsc::channel::<T>(processing_queue_capacity);
+        let (new_urls_x1, mut new_urls_y1) =
+            tokio::sync::mpsc::channel::<T>(crawling_queue_capacity);
+        let barrier_x1 = Arc::new(Barrier::new(1));
+
+        for url in handler.url() {
+            visited_urls.insert(url.clone());
+            // let _ = urls_to_visit_x1.send().await;
+        }
+
+        loop {
+            if new_urls_y1.try_recv().ok().is_some() {
+                for url in handler.url() {
+                    visited_urls.insert(url.clone());
+                    // let _ = urls_to_visit_x1.send().await;
+                    if !visited_urls.contains(&url) {
+                        visited_urls.insert(url.clone());
+                        // let _ = urls_to_visit_x1.send(url).await;
+                    }
+                }
+            }
+
+            if new_urls_x1.capacity() == crawling_queue_capacity
+                && urls_to_visit_x1.capacity() == crawling_queue_capacity
+                && active_crawlers.load(Ordering::SeqCst) == 0
+            {
+                // no more work, we leave
+                break;
+            }
+            tokio::time::sleep(std::time::Duration::from_millis(8))/* .await */;
+        }
     }
 }
 
